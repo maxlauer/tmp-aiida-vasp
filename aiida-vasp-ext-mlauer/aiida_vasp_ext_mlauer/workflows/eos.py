30eb6a4970250d04dabe5448e3821b5e4a488786
sm2858 <Maximilian.Lauer@materialwiss.uni-giessen.de>
2025-10-23 17:59:31 +0200

    EoSWorkChain almost finished, only plotting doesn't work yet .. tomorrow

--- aiida-vasp-ext-mlauer/aiida_vasp_ext_mlauer/workflows/eos.py
+++ aiida-vasp-ext-mlauer/aiida_vasp_ext_mlauer/workflows/eos.py
@@ -1,5 +1,9 @@
 import numpy as np
-import random
+import io 
+
+from scipy.interpolate import CubicSpline
+from scipy.optimize import minimize, curve_fit
+from matplotlib import pyplot as plt
 
 from aiida.engine import WorkChain, calcfunction, while_, append_
 from aiida.common.extendeddicts import AttributeDict
@@ -8,17 +12,22 @@ from aiida.plugins import DataFactory, WorkflowFactory
 from aiida_vasp.utils.workchains import prepare_process_inputs # Should check to see that the inputs given to vasp.vasp works well .. not too sure
 from aiida_vasp.utils.workchains import compose_exit_code # combines exit codes together
 
-from scipy.interpolate import interp1d
-from scipy.optimize import minimize
+from aiida.orm import Dict
 
-## THIS IS A STUB .. DOESN"T WORK ...
+from aiida_vasp_ext_mlauer.util.functions import Murnaghan
+
+_DEFAULT_K0 = 10.0
+_DEFAULT_K1 = 4.5
+
+
 
 class EoSWorkChain(WorkChain):
     """
-    The Equation of State Work Chain
+    The Parallel Equation of State Work Chain 
     ----------------------------------
 
-    The WorkChain accepts a dictionary of structures and extracteds
+    The WorkChain accepts a dictionary of structures and extracteds.
+    Compared to the normal EoSWorkChain, this WorkChain runs all calculations in parallel.
     """
 
     _verbose = False 
@@ -35,6 +44,26 @@ class EoSWorkChain(WorkChain):
                              dynamic = True,
                              help = "Dictionary of Structures used for the Equation of State")
         
+        spec.input('minimum_mode',
+                   valid_type = DataFactory("core.str"),
+                   default = lambda: DataFactory("core.str")('Interpolate'), 
+                   help = "Determines how the energy minimum is evaluated - Interpolate: 1D cubic Interpolation; Murnaghan: fit Murnaghan EoS"
+                )
+        
+
+        spec.input_namespace(name='wc_metadata',
+                             dynamic = False,
+                             help='Custom namespace for WorkChain-specific metadata (e.g. verbosity, plotting, tags).'
+                )
+        
+        spec.input('wc_metadata.create_plot',
+                   valid_type = bool,
+                   default = True,
+                   help = "Toggle whether to create a plot, or not. Default is False",
+                   non_db = True
+                )
+        
+
         # Specify Exit Codes
         spec.exit_code(0, "NO_ERROR", message="Calculation ended without Error ... well done I guess")
         spec.exit_code(420, "ERROR_NO_CALLED_WORKCHAIN", message="No called WorkChain detected")
@@ -42,18 +71,15 @@ class EoSWorkChain(WorkChain):
 
         spec.outline(
             cls.initialize,
-            while_(cls.run_next_workchains)(
-                cls.init_next_workchain,
-                cls.run_next_workchain,
-                cls.verify_next_workchain,
-                cls.extract_volume_and_energy
-            ),
+            cls.init_and_run_next_workchains,
+            cls.verify_next_workchain,
+            cls.extract_volume_and_energy,
             cls.finalize
         )
 
         spec.output('eos', valid_type=DataFactory('core.array'), help='A List of Cell Volumes and Total Energies')
-        # spec.output('eos_plot', valid_type=DataFactory('core.singlefile'), help='A plot visualizing the Volume-Total Energy relationship, with the fitted EoS')
-        # spec.output('eos_parameter', valid_type=DataFactory('core.dict'), help="A Dict containing the all parameters and the 'type' of the fitted EoS")
+        spec.output('eos_plot', valid_type=DataFactory('core.singlefile'), required = False, help='A plot visualizing the Volume-Total Energy relationship, with the fitted EoS')
+        spec.output('eos_parameter', valid_type=DataFactory('core.dict'), help="A Dict containing the all parameters and the 'type' of the fitted EoS")
         spec.output('eos_minimum', valid_type=DataFactory('core.dict'), help="A cell volume containing the cell volume and total energy at energy minimum")
 
 
@@ -70,9 +96,11 @@ class EoSWorkChain(WorkChain):
         # Load Structures in Context
         self.ctx.structures = dict(self.inputs.structures)
 
-        self.ctx.is_finished = False
-
         self.ctx.iteration = 0
+
+        self.ctx.mode = self.inputs.minimum_mode.value
+
+        self.ctx.wc_metadata = self.inputs.wc_metadata
 
         # define context inputs
         self.ctx.inputs = AttributeDict()
@@ -89,19 +117,8 @@ class EoSWorkChain(WorkChain):
             pass 
 
 
+    def init_and_run_next_workchains(self):
 
-    def run_next_workchains(self):
-        """
-        Return whether a new WorkChain should be run
-        
-        Returns True, if the last WorkChain has not finished successfully 
-        """
-        return not self.ctx.is_finished
-
-
-    def init_next_workchain(self):
-
-        self.ctx.iteration += 1
 
         # Check existence of context inputs
         try:
@@ -115,17 +132,19 @@ class EoSWorkChain(WorkChain):
 
         # Only missing is  `structure`, which we didn't expose
         # choose randomly from `structures` entry point 
-        item = random.choice(list(self.inputs.structures.keys()))
-        self.ctx.inputs.structure = self.ctx.structures.pop(item)
+        for structure_key, structure in self.ctx.structures.items():
+            print(structure_key, structure)
 
-        self.ctx.inputs = prepare_process_inputs(self.ctx.inputs, namespaces=['dynamics'])
+            # Increase iteration index, update structure of ctx.inputs and make sure the inputs are correctly formatted
+            self.ctx.iteration += 1
+            self.ctx.inputs.structure = structure
+            self.ctx.inputs = prepare_process_inputs(self.ctx.inputs, namespaces=['dynamics'])
 
-    
-    def run_next_workchain(self):
-        inputs = self.ctx.inputs
-        running = self.submit(self._next_workchain, **inputs)
-        self.report(f'launching {self._next_workchain.__name__}<{running.pk}> iteration #{self.ctx.iteration}')
-        self.to_context(workchains=append_(running))
+            # Submit VaspWorkChain for Structure
+            running = self.submit(self._next_workchain, **self.ctx.inputs)
+
+            self.report(f'launching {self._next_workchain.__name__}<{running.pk}> iteration #{self.ctx.iteration}')
+            self.to_context(workchains=append_(running))
 
 
     def verify_next_workchain(self):
@@ -136,50 +155,65 @@ class EoSWorkChain(WorkChain):
             self.report(f"There is no {self._next_workchain.__name__} in the called workchain list.")
             return self.exit_codes.ERROR_NO_CALLED_WORKCHAIN
         
-        # Inherit exit status from last workchain
-        next_workchain_exit_status = workchain.exit_status
-        next_workchain_exit_message = workchain.exit_message
+        for workchain in self.ctx.workchains:
+            # Inherit exit status from last workchain
+            next_workchain_exit_status = workchain.exit_status
+            next_workchain_exit_message = workchain.exit_message
 
-        # if last workchain successful
-        if not next_workchain_exit_status:
-            self.ctx.exit_code = self.exit_codes.NO_ERROR
+            # if last workchain successful
+            if not next_workchain_exit_status:
+                self.ctx.exit_code = self.exit_codes.NO_ERROR
 
-        else:
-            self.ctx.exit_code = compose_exit_code(next_workchain_exit_status, next_workchain_exit_message)
-            self.report(f"The called{workchain.__class__.__name__}<{workchain.pk}> returned a non-zero exit status.\nThe exit status {self.ctx.exit_code} is inherited.")
+            else:
+                self.ctx.exit_code = compose_exit_code(next_workchain_exit_status, next_workchain_exit_message)
+                self.report(f"The called{workchain.__class__.__name__}<{workchain.pk}> returned a non-zero exit status.\nThe exit status {self.ctx.exit_code} is inherited.")
 
         
-        if not self.ctx.structures:
-            self.ctx.is_finished = True 
+            if not self.ctx.structures:
+                self.ctx.is_finished = True 
 
         return self.ctx.exit_code
     
 
 
     def extract_volume_and_energy(self):
-        """
-        Extract cell volume and total energy from the respective structure
-        """
+        """Extract the cell volume and total energy for this structure."""
 
-        workchain = self.ctx.workchains[-1]
-        misc = workchain.outputs.misc.get_dict()
-        total_energy = misc['total_energies']['energy_extrapolated']
+        for workchain in self.ctx.workchains:
+            # Fetch the total energy
+            misc = workchain.outputs.misc.get_dict()
+            total_energy = misc['total_energies']['energy_extrapolated']
 
-        # Get Volume
-        volume = self.ctx.inputs.structure.get_cell_volume()
+            # Fetch the volume
+            self.report(f"{workchain.inputs}")
+            volume = workchain.inputs.structure.get_cell_volume()
 
-        # Store
-        self.ctx.total_energies.append([volume, total_energy])
+            # Store both in a list
+            self.ctx.total_energies.append([volume, total_energy])
 
     
     def finalize(self):
         # To get DataProvenance, the AiiDA List container has to be prepared by a calcfunction
         total_energies = store_total_energies(DataFactory('list')(list=self.ctx.total_energies))
 
-        energy = locate_minimum(total_energies)
+
+        if self.ctx.mode == "Interpolate":
+            analysis_dict = locate_minimum_interpolate(total_energies)
+            self.report(f"Minimum Energy Determination using 1D cubic Interpolation")
+        elif self.ctx.mode == "Murnaghan":
+            analysis_dict = locate_minimum_murnaghan(total_energies)
+            self.report(f"Minimum Energy Determination fitting the Murnaghan Equation of State")
+            
+
+        if self.ctx.wc_metadata.create_plot:
+            self.report("Generating Plot of Equation of State")
+            create_plot(total_energies, parameters=analysis_dict["min_energy"])
 
         self.out('eos', total_energies)
-        self.out('eos_minimum', energy)
+        self.out('eos_minimum', analysis_dict["min_energy"])
+        self.out('eos_parameter', analysis_dict["min_energy"])
+
+
 
 
 
@@ -197,8 +231,9 @@ def store_total_energies(total_energies):
     return array_data
 
 
+
 @calcfunction
-def locate_minimum(total_energies):
+def locate_minimum_interpolate(total_energies):
     total_energies_array = total_energies.get_array('eos')
 
     volumes = total_energies_array[:,0]
@@ -207,12 +242,101 @@ def locate_minimum(total_energies):
     ## get minimum energy guess
     min_energy_guess_idx = energies.argmin()
 
-
-    # for now cubic interpolation - later on implement murnaghan equation fitting, or other EOS or other interpolation schemes
-    new_energies = interp1d(volumes, energies, kind='cubic')
-    min_energy_point = minimize(new_energies, volumes[min_energy_guess_idx], tol=1e-3)
+    new_energies = CubicSpline(volumes, energies)
+    min_energy_point = minimize(new_energies, volumes[min_energy_guess_idx], tol=1e-3)  # I should make these numbers optional under wc_metadata.plot_options
 
     # Store Result
-    dict_data = DataFactory('dict')(dict = {'volume': min_energy_point.x[0], 'energy': min_energy_point.fun})
+    dict_data = {'volume': min_energy_point.x[0], 'energy': min_energy_point.fun}
 
-    return dict_data 
+    parameters = {'type': '1D cubic Interpolation'}
+
+    return {
+        "min_energy": Dict(dict = dict_data), 
+        "parameters": Dict(dict = parameters) 
+    }
+
+
+
+@calcfunction
+def locate_minimum_murnaghan(total_energies):
+    total_energies_array = total_energies.get_array('eos')
+
+    volumes = total_energies_array[:,0]
+    energies = total_energies_array[:,1]
+
+    # Document Type of Analysis
+    parameters = {'type': 'Murnaghan EoS Fit'}
+
+    ## get minimum energy guess
+    min_energy_guess_idx = energies.argmin()
+
+    ## Fit Murnaghan Equation of State
+    params, covar = curve_fit(Murnaghan, volumes, energies, p0=[volumes[min_energy_guess_idx], energies[min_energy_guess_idx], _DEFAULT_K0, _DEFAULT_K1])
+    parameters |= {'E0': params[0], 
+                   'V0': params[1], 
+                   'K0': params[2], 
+                   'K1': params[3],
+                   #'covariance': covar.tolist()
+                   }
+
+    min_energy_point = Murnaghan(params[1], *params)
+
+    # Store Result
+    dict_data = {'volume': params[0], 'energy': min_energy_point}
+
+    return {
+        "min_energy": Dict(dict = dict_data), 
+        "parameters": Dict(dict = parameters) 
+    }
+
+
+
+@calcfunction
+def create_plot(total_energies, parameters):
+    total_energies_array = total_energies.get_array('eos')
+    parameters = parameters.get_dict()
+
+    volumes = total_energies_array[:,0]
+    energies = total_energies_array[:,1]
+    type = parameters['type'] 
+
+    x_fit = np.linspace(volumes.min - 1, volumes.max() + 1, 500)
+
+    fig, ax = plt.subplots()
+    ax.scatter(volumes, energies)
+
+    match type:
+        case "Murnaghan EoS Fit":
+            params = [parameters.get(para) for para in ['E0', 'V0', 'K0', 'K1']]
+            v0 = parameters.get('V0')
+        
+            ax.set_title('Murnaghan EoS Plot') # I should make these numbers optional under wc_metadata.plot_options
+            y_fit = Murnaghan(x_fit, *params)
+
+            ax.plot(x_fit, y_fit, label='Murnaghan Fit', color='orange')
+            
+
+        case "1D cubic Interpolation":
+            min_energy_guess_idx = energies.argmin()
+            y_fit = CubicSpline(volumes, energies)
+            
+            v0 = minimize(y_fit, min_energy_guess_idx, tol=1e-3)
+
+            ax.set_title("Interpolated EoS Plot")
+            ax.plot(x_fit, y_fit)
+
+    ax.axvline(x=v0, color='grey', linestyle='--', label=fr'$V_0 = {v0:.3f}$ $A^3$')
+
+    ax.set_xlabel(r'Volume / $10^\mathrm{-30}$ m^\mathrm{3}')
+    ax.set_ylabel(r'Energy / eV')
+    ax.legend()
+
+    buffer = io.BytesIO()
+    fig.savefig(buffer, format='png')
+    fig.close()
+
+    buffer.seek(0)
+
+    plot_file = DataFactory('core.singlefile')(file=buffer, filename='eos_plot.png')
+
+    return plot_file
